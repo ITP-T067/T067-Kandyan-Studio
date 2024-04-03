aee5fa655056861cad3a8353f1ddcc453cb2ecc5
Sandali Dias <ssd.dias29@gmail.com>
2024-04-03 23:34:28 +0530

    Backend folder structure

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 8010
+
+mongoose.connect("mongodb+srv://ssddias29:kandyan123@kandyan-studio.qgnehe2.mongodb.net/?retryWrites=true&w=majority&appName=Kandyan-Studio")
+.then(() => {
+    console.log("Connect to DB")
+    app.listen(PORT, () => console.log("Server is running..."))
+})
+.catch((err) => console.log(err))
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({success: false, statusCode, message});
+ }); 
