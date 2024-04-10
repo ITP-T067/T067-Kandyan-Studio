1e44a8966cd6f05c79ed863132b8d31f4e2b5d72
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2024-04-10 21:03:11 +0530

    Customer Header Added, Login Page Modified

--- frontend/src/pages/customer/customerHeader.jsx
+++ frontend/src/pages/customer/customerHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import '../../Styles/style.css';
-import { HiOutlineLogout,HiOutlineShoppingCart } from "react-icons/hi";
+import { HiOutlineLogout,HiOutlineShoppingCart,HiBookmark } from "react-icons/hi";
 
 const CustomerHeader = () => {
     const userRole = localStorage.getItem('userRole');
@@ -27,14 +27,20 @@ const CustomerHeader = () => {
             </div>
             <img src={logo} alt="Logo" width="30" height="30" className="mx-auto" />
             <button className="actor-button text-kwhite font-bold py-2 px-4 rounded-full"><HiOutlineShoppingCart className='w-6 h-6'/></button>
-            <div className="navbar-buttons flex">
-                <div>
-                <div class="flex-shrink-0">
-                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                </div>
-                <button className="actor-button text-kwhite font-bold py-2 px-4 rounded">{username}</button>
-                </div>
-                <button className="logout-button text-kwhite font-bold py-2 px-4 rounded" onClick={handleLogout}><HiOutlineLogout /></button>
+            <div className="flex">
+                <button className="flex bg-kblack text-kwhite">
+                    <img class="ml-5 h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    <div className='ml-3'>
+                        {username}
+                        <div className='flex'>
+                        <HiBookmark/>
+                        <span className='text-xs flex'>Loyalty</span>
+                        </div>
+                    </div>
+                </button>
+                <button className="ml-5 px-4 logout-button items-center justify-center text-kwhite rounded-full" onClick={handleLogout}>
+                    <HiOutlineLogout />
+                    </button>
             </div>
         </nav>
     );
