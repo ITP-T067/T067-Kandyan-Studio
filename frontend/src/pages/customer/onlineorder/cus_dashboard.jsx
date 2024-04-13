b542cf7fa5911d9c43085b263cf74f8fedf17360
Prasad Chandima <pcsplus23@gmail.com>
2024-04-14 02:38:22 +0530

    alert box create

--- frontend/src/pages/customer/onlineorder/cus_dashboard.jsx
+++ frontend/src/pages/customer/onlineorder/cus_dashboard.jsx
@@ -2,25 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { FaSearch } from "react-icons/fa";
 import '../../../Styles/cus_dashboard.css';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import  Axios from 'axios';
 function Cus_dashboard() {
 
   const [items, setItems] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    async function fetchItems() {
-      try {
-        const response = await axios.get('/item'); // Assuming your backend endpoint is '/api/getitem'
-        const { data } = response.data;
-        setItems(data);
-      } catch (error) {
-        console.error('Error fetching items:', error);
-      }
-    }
-    fetchItems();
-  }, []);
+    getItems();
+  },[]);
 
+  const getItems = () =>{
+    Axios.get('http://localhost:8010/item')
+      .then(response => {
+        setItems(response.data.data);
+      })
+      .catch(error =>{
+        console.error('Error fetching items:', error);
+      })
+  }
+
+  
   return (
     <>
       <div className='Cus_dashboard flex'>
@@ -34,13 +36,13 @@ function Cus_dashboard() {
         {/* Container for cards */}
         <div class="flex flex-wrap justify-center gap-5 mt-5">
         {items.map(item => (
-          <div key={item._id} className="card w-64 h-80 relative cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105" onClick={() => navigate('/addtocart')}>
+          <div key={item._id} className="card w-64 h-80 relative cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105" onClick={() => navigate(`/addtocart`)}>
             <div class="w-64 h-80 absolute bg-kgray opacity-50 rounded-3xl"></div>
             <img className="absolute rounded-3xl" src={item.image} style={{ width: '225px', height: '225px', left: '16px', top: '20px' }} alt="Photography" />
             <div className="Souvenir absolute text-white text-center text-2xl font-bold top-64 left-0 right-0">{item.name}</div>
           </div>
          ))} 
-        </div>
+         </div>
         
       </div>
     </>
