3b8153085f2c6af41837eab9da9117665550abc0
Prasad Chandima <pcsplus23@gmail.com>
2024-04-18 03:49:21 +0530

    Update cus_dashboard.jsx

--- frontend/src/pages/customer/onlineorder/cus_dashboard.jsx
+++ frontend/src/pages/customer/onlineorder/cus_dashboard.jsx
@@ -58,7 +58,7 @@ function Cus_dashboard() {
         {filteredItems.map(item => (
           <div key={item._id} className="card w-64 h-80 relative cursor-pointer transition-transform duration-300 ease-in-out hover:scale-105" onClick={() => handleCardClick(item._id)}>
             <div class="w-64 h-80 absolute bg-kgray opacity-50 rounded-3xl"></div>
-            <img className="absolute rounded-3xl" src={item.image} style={{ width: '225px', height: '225px', left: '16px', top: '20px' }} alt="Photography" />
+            <img className="absolute rounded-3xl" src={item.image} style={{ width: '225px', height: '225px', left: '16px', top: '20px' }} alt={item.image} />
             <div className="Souvenir absolute text-white text-center text-2xl font-bold top-64 left-0 right-0">{item.name}</div>
           </div>
          ))} 
