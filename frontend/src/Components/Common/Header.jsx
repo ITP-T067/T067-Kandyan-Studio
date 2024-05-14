cb2d3ba9dc26479186524b80a608f5b8c0b89b30
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2024-05-15 00:41:12 +0530

    View Pay Slip Section Added

--- frontend/src/Components/Common/Header.jsx
+++ frontend/src/Components/Common/Header.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import logo from '../../images/logo.png';
 import '../../Styles/style.css';
 import Customer from '../../pages/customer/customerHeader';
-import { HiOutlineLogout,HiCurrencyDollar } from "react-icons/hi";
-
+import { HiOutlineLogout, HiCurrencyDollar } from "react-icons/hi";
 
 const Header = () => {
     const userRole = localStorage.getItem('userRole');
@@ -13,29 +12,53 @@ const Header = () => {
         localStorage.setItem('userRole', '');
         localStorage.setItem('username', '');
         localStorage.setItem('userID', '');
-        localStorage.setItem('activeButton',null);
+        localStorage.setItem('activeButton', null);
 
         console.log('User role:', userRole);
         console.log('User session destroyed');
         window.location.href = '/login';
     };
 
+    const handleViewPaySlip = (actor) => {
+        if (actor === 'Cashier') {
+            window.location.href = '/cashier/cashierpayslip';
+        } else if (actor === 'Creator') {
+            window.location.href = '/creator/creatorpayslip';
+        } else if (actor === 'Studio Operator') {
+            window.location.href = '/studiooperator/studioslip';
+        } else {
+            console.log('You are not authorized to view pay slip');
+        }
+        console.log('View Pay Slip');
+    }
+
     const renderHeader = () => {
         if (userRole === 'Customer') {
             return <Customer />;
-        } else if (userRole === 'Manager' || userRole === 'Cashier' || userRole === 'Creator' || userRole === 'Supplier' || userRole === 'Studio Operator') {
+        } else if (userRole === 'Cashier' || userRole === 'Creator' || userRole === 'Supplier' || userRole === 'Studio Operator') {
             return (
                 <nav className="navbar flex justify-between items-center px-20 top-0 left-0 bg-black py-30">
                     <div className='navbar-title text-kwhite text-4xl font-bold'>{userRole} Dashboard</div>
                     <img src={logo} alt="Logo" width="30" height="30" className="mx-auto" />
                     <div className="flex flex-cols">
-                        <button className="flex items-center space-x-2 text-sm text-kblack bg-kwhite font-bold py-2 px-4 rounded-full"><HiCurrencyDollar/>View Pay Slip</button>
+                        <button className="flex items-center space-x-2 text-sm text-kblack bg-kwhite font-bold py-2 px-4 rounded-full" onClick={() => handleViewPaySlip(userRole)}><HiCurrencyDollar />View Pay Slip</button>
                         <button className="actor-button text-kwhite text-sm font-bold py-2 px-4 rounded-full mr-2 ml-2">{username}</button>
-                        <button className="flex items-center space-x-2 text-sm logout-button text-kwhite font-bold py-2 px-4 rounded-full" onClick={handleLogout} ><HiOutlineLogout /><span className="text-sm">Logout</span></button>
+                        <button className="flex items-center space-x-2 text-sm logout-button text-kwhite font-bold py-2 px-4 rounded-full" onClick={handleLogout}><HiOutlineLogout /><span className="text-sm">Logout</span></button>
                     </div>
                 </nav>
             );
-        } else {
+        } else if (userRole === 'Manager'){
+            return (
+                <nav className="navbar flex justify-between items-center px-20 top-0 left-0 bg-black py-30">
+                    <div className='navbar-title text-kwhite text-4xl font-bold'>{userRole} Dashboard</div>
+                    <img src={logo} alt="Logo" width="30" height="30" className="mx-auto" />
+                    <div className="flex flex-cols">
+                        <button className="actor-button text-kwhite text-sm font-bold py-2 px-4 rounded-full mr-2 ml-2">{username}</button>
+                        <button className="flex items-center space-x-2 text-sm logout-button text-kwhite font-bold py-2 px-4 rounded-full" onClick={handleLogout}><HiOutlineLogout /><span className="text-sm">Logout</span></button>
+                    </div>
+                </nav>
+            );
+        }else {
             return null;
         }
     };
