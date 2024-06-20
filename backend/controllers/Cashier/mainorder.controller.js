0a5e4d6a16e95a304c4898640dc424388602dad8
Pahan-Samudika <pahanabhayawardhane@gmail.com>
2024-06-20 22:11:46 +0530

    Fixed Observed Runtime Errors

--- backend/controllers/Cashier/mainorder.controller.js
+++ backend/controllers/Cashier/mainorder.controller.js
@@ -2,59 +2,54 @@ const Mainorder = require("../../models/Cashier/mainorder.model.js");
 const {errorHandler} = require("../../utils/error.js");
 
 //create mainorder
-const create_Mainorder = async(req,res, next) => {
-    console.log(req.body)
-    const data = new Mainorder(req.body)
-    try{
-        await data.save() 
-        if(res.status(201)){
-            res.send({success : true, message : "mainorder saved successfully", data: data})
-        }
-    }catch(error){
-        next(error);
+const create_Mainorder = async (req, res, next) => {
+    try {
+        const data = new Mainorder(req.body);
+        await data.save();
+        res.status(201).send({ success: true, message: "Mainorder saved successfully", data: data });
+    } catch (error) {
+        next(errorHandler(error));
     }
-}
+};
 
 //read mainorder
-const index_Mainorder = async(req, res, next) => {
+const index_Mainorder = async (req, res, next) => {
     try {
         const data = await Mainorder.find();
-        if(res.status(201)){
-            res.json({success : true , data: data});
-        }
+        res.status(200).json({ success: true, data: data });
     } catch (error) {
-        next(error);
+        next(errorHandler(error));
     }
-}
+};
 
 //delete mainorder
-const del_Mainorder = async(req,res, next) =>{
-    const id = req.params.id
-    console.log(id)
-
-    try{
-        const data = await Mainorder.deleteOne({_id : id})
-        if(res.status(201)){
-            res.send({success:true, message: "Mainorder deleted successfully", data : data})
+const del_Mainorder = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const data = await Mainorder.deleteOne({ _id: id });
+        if (data.deletedCount > 0) {
+            res.status(200).send({ success: true, message: "Mainorder deleted successfully", data: data });
+        } else {
+            res.status(404).send({ success: false, message: "Mainorder not found" });
         }
-    }catch(error){
-        next(error);
+    } catch (error) {
+        next(errorHandler(error));
     }
-}
+};
 
 //update mainorder
-const update_Mainorder = async(req, res, next) => {
-    console.log(req.body);
-    const {_id, ...rest} = req.body;
-    console.log(rest);
+const update_Mainorder = async (req, res, next) => {
+    const { _id, ...rest } = req.body;
     try {
-        const data = await Mainorder.updateOne({_id : _id}, rest);
-        if(res.status(201)){
-            res.send({success:true, message: "Mainorder updated successfully", data : data});
+        const data = await Mainorder.updateOne({ _id: _id }, rest);
+        if (data.nModified > 0) {
+            res.status(200).send({ success: true, message: "Mainorder updated successfully", data: data });
+        } else {
+            res.status(404).send({ success: false, message: "Mainorder not found" });
         }
-    }catch(error){
-        next(error);
+    } catch (error) {
+        next(errorHandler(error));
     }
-}
+};
 
 module.exports = { create_Mainorder, index_Mainorder, del_Mainorder,update_Mainorder};
